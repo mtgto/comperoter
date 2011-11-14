1bb8bac4b8fafdb23b0fe9379a273bd35185f1de
mtgto <hogerappa@gmail.com>
2011-11-15 01:49:31 +0900

    式のパースで加減乗除の繰り返しに対応

--- prpr.scala
+++ prpr.scala
@@ -35,19 +35,32 @@ case class Modulo(exp1:Exp, exp2:Exp) extends Exp
 
 class MyParser extends JavaTokenParsers {
   def expr: Parser[Exp] =
-    (term ~ "+" ~ term) ^^ {case a~"+"~b => Add(a, b)} |
-  (term ~ "-" ~ term) ^^ {case a~"-"~b => Sub(a, b)} |
-  term
-
+    term ~ rep(("+" | "-") ~ term) ^^ {
+      case a ~ rest => {
+	rest.foldLeft(a)(
+	  (left, right) => right match {
+	    case "+"~right => Add(left, right)
+	    case "-"~right => Sub(left, right)
+	  }
+	)
+      }
+    }
+  
   def term: Parser[Exp] =
-    (factor ~ "*" ~ factor) ^^ {case a~"*"~b => Multiply(a, b)} |
-  (factor ~ "/" ~ factor) ^^ {case a~"/"~b => Divide(a, b)} |
-  (factor ~ "%" ~ factor) ^^ {case a~"%"~b => Modulo(a, b)} |
-  factor
+    factor ~ rep(("*" | "/" | "%") ~ factor) ^^ {
+      case a ~ rest => {
+	rest.foldLeft(a)(
+	  (left, right) => right match {
+	    case "*"~right => Multiply(left, right)
+	    case "/"~right => Divide(left, right)
+	    case "%"~right => Modulo(left, right)
+	  }
+	)
+      }
+    }
 
   def factor: Parser[Exp] =
-    "("~>expr<~")" |
-  floatingPointNumber ^^ {case a => Num(a.toFloat)}
+    "("~>expr<~")" | floatingPointNumber ^^ {case a => Num(a.toFloat)}
 
   def parse(data:String) {
     println(parseAll(expr, data))
@@ -64,7 +77,7 @@ object prpr {
   }
 }
 val parser = new MyParser
-//parser.parse("1+(2+3)*4+5+(6/7)")
-//parser.parse("1+2")
-//parser.parse("1+2-3")
+parser.parse("1+(2+3)*4+5+(6/7)")
+parser.parse("1+2")
+parser.parse("1+2-3")
 parser.parse("1*2*3*4")
