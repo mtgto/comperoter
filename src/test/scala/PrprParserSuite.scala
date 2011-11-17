194c12e7d7e1b0170c97c9761f653d0b48a0b735
mtgto <hogerappa@gmail.com>
2011-11-18 01:59:45 +0900

    コンパイラのテスト（定数）を追加

--- src/test/scala/PrprParserSuite.scala
+++ src/test/scala/PrprParserSuite.scala
@@ -12,6 +12,25 @@ class PrprParserSuite extends FunSuite {
 
     assert(parsedExpr("1"))
     assert(!parsedExpr("1.0"))
-    assert(parsedExpr("-1"))
+    assert(!parsedExpr("-1"))
+  }
+  
+  test("compile") {
+    val parser = new MyParser
+    val parsedExpr = (in:String) =>
+      parser.parseAll(parser.expr, in) match {
+	case parser.Success(expr, _) => expr
+	case _ => throw new RuntimeException("failed to parse expression: " + in)
+      }
+    
+    val compiler = new MyPrprCompiler
+    val prpr = compiler.prpr
+    val zero = compiler.zero
+    val one = compiler.one
+
+    assert(prpr + one + zero + prpr == compiler.convertExpr(parsedExpr("0"), List()))
+    assert(prpr + one + one + prpr == compiler.convertExpr(parsedExpr("1"), List()))
+    assert(prpr + one + one + zero + prpr == compiler.convertExpr(parsedExpr("2"), List()))
+    assert(prpr + one + one + zero + zero + prpr == compiler.convertExpr(parsedExpr("4"), List()))
   }
 }
