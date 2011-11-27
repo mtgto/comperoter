102cb5deccf5927ace93c5310afc27e356ab9191
mtgto <hogerappa@gmail.com>
2011-11-28 00:51:48 +0900

    ペログラム言語の実行機を実装。

--- src/main/scala/prpr/Command.scala
+++ src/main/scala/prpr/Command.scala
@@ -2,23 +2,38 @@ package prpr
 
 sealed trait PrprCommand
 
-case class Push(num: Float) extends PrprCommand
-case class Dup() extends PrprCommand
-case class Swap() extends PrprCommand
-case class Pop() extends PrprCommand
-case class Add() extends PrprCommand
-case class Sub() extends PrprCommand
-case class Mul() extends PrprCommand
-case class Div() extends PrprCommand
-case class Mod() extends PrprCommand
-case class Store() extends PrprCommand
-case class Load() extends PrprCommand
-case class Label(label: Float) extends PrprCommand
-case class Jump(label: Float) extends PrprCommand
-case class JZero(label: Float) extends PrprCommand
-case class JNeg(label: Float) extends PrprCommand
-case class End() extends PrprCommand
-case class PrintChar() extends PrprCommand
-case class PrintNum() extends PrprCommand
-case class ReadChar() extends PrprCommand
-case class ReadNum() extends PrprCommand
+/**
+ * スタックに値が不要なコマンド
+ */
+sealed trait ZeroStackCommand extends PrprCommand
+
+/**
+ * スタックに1つ以上値が必要なコマンド
+ */
+sealed trait OneStackCommand extends PrprCommand
+
+/**
+ * スタックに2つ以上値が必要なコマンド
+ */
+sealed trait TwoStackCommand extends PrprCommand
+
+case class Push(num: Float) extends ZeroStackCommand
+case class Dup() extends OneStackCommand
+case class Swap() extends TwoStackCommand
+case class Pop() extends OneStackCommand
+case class Add() extends TwoStackCommand
+case class Sub() extends TwoStackCommand
+case class Mul() extends TwoStackCommand
+case class Div() extends TwoStackCommand
+case class Mod() extends TwoStackCommand
+case class Store() extends TwoStackCommand
+case class Load() extends OneStackCommand
+case class Label(label: Float) extends ZeroStackCommand
+case class Jump(label: Float) extends ZeroStackCommand
+case class JZero(label: Float) extends OneStackCommand
+case class JNeg(label: Float) extends OneStackCommand
+case class End() extends ZeroStackCommand
+case class PrintChar() extends OneStackCommand
+case class PrintNum() extends OneStackCommand
+case class ReadChar() extends ZeroStackCommand
+case class ReadNum() extends ZeroStackCommand
--- src/main/scala/prpr/Runner.scala
+++ src/main/scala/prpr/Runner.scala
@@ -18,9 +18,114 @@ class Runner(prpr:String) {
     parseResult match {
       case parser.Success(commands, _) =>
 	val labelMap = generateLabelMap(commands, Map())
-
+	executeCommands(commands, List(), Map(), labelMap)
       case _ =>
 	throw new RuntimeException("failed to parse the program.")
+    }
+  }
+
+  def executeCommands(commands:List[PrprCommand], stack:List[Float], heap:Map[Int, Float], labelMap:Map[Float, List[PrprCommand]]): Unit = {
+    commands match {
+      case hd::tl => {
+	hd match {
+	  case command:ZeroStackCommand =>
+	    command match {
+	      case Push(num) =>
+		executeCommands(tl, num::stack, heap, labelMap)
+	      case Label(label) =>
+		executeCommands(tl, stack, heap, labelMap)
+	      case Jump(label) =>
+		labelMap.get(label) match {
+		  case Some(list) =>
+		    executeCommands(list, stack, heap, labelMap)
+		  case _ =>
+		    throw new RuntimeException("no label is defined")
+		}
+	      case End() =>
+		println("finished")
+	      case ReadChar() =>
+		print("Please input character: ")
+		val char = readLine().head.toFloat
+		executeCommands(tl, char::stack, heap, labelMap)
+	      case ReadNum() =>
+		print("Please input number: ")
+		val num = readLine().toInt
+		executeCommands(tl, num::stack, heap, labelMap)
+	    }
+	  case command:OneStackCommand =>
+	    stack match {
+	      case fst::rest =>
+		command match {
+		  case Dup() =>
+		    executeCommands(tl, fst::fst::rest, heap, labelMap)
+		  case Pop() =>
+		    executeCommands(tl, rest, heap, labelMap)
+		  case Load() =>
+		    heap.get(fst.toInt) match {
+		      case Some(value) => executeCommands(tl, value::rest, heap, labelMap)
+		      case _ => throw new RuntimeException("invalid indexes: " + fst.toInt)
+		    }
+		  case JZero(label) =>
+		    labelMap.get(label) match {
+		      case Some(list) =>
+			if (fst == 0)
+			  executeCommands(list, rest, heap, labelMap)
+			else
+			  executeCommands(tl, rest, heap, labelMap)
+		      case _ =>
+			throw new RuntimeException("no label is defined")
+		    }
+		  case JNeg(label) =>
+		    labelMap.get(label) match {
+		      case Some(list) =>
+			if (fst < 0)
+			  executeCommands(list, rest, heap, labelMap)
+			else
+			  executeCommands(tl, rest, heap, labelMap)
+		      case _ =>
+			throw new RuntimeException("no label is defined")
+		    }
+		  case PrintChar() =>
+		    print(fst.toChar); executeCommands(tl, stack, heap, labelMap)
+		  case PrintNum() =>
+		    print(fst.toInt); executeCommands(tl, stack, heap, labelMap)
+		}
+	      case _ =>
+		throw new RuntimeException("stack is empty")
+	    }
+	  case command:TwoStackCommand =>
+	    stack match {
+	      case fst::snd::rest => {
+		command match {
+		  case Swap() =>
+		    executeCommands(tl, snd::fst::rest, heap, labelMap)
+		  case Add() =>
+		    executeCommands(tl, (snd+fst)::rest, heap, labelMap)
+		  case Sub() =>
+		    executeCommands(tl, (snd-fst)::rest, heap, labelMap)
+		  case Mul() =>
+		    executeCommands(tl, (snd*fst)::rest, heap, labelMap)
+		  case Div() =>
+		    if (snd == 0)
+		      throw new RuntimeException("divide zero")
+		    else
+		      executeCommands(tl, (snd/fst)::rest, heap, labelMap)
+		  case Mod() =>
+		    if (snd == 0)
+		      throw new RuntimeException("divide zero")
+		    else
+		      executeCommands(tl, (snd%fst)::rest, heap, labelMap)
+		  case Store() =>
+		    executeCommands(tl, rest, heap.updated(fst.toInt, snd), labelMap)
+		}
+	      }
+	      case _ =>
+		throw new RuntimeException("stack must have at leaset two values")
+	    }
+	}
+      }
+      case _ =>
+	println("finished")
     }
   }
 }
